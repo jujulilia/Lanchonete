a50897d2f8572c985e2022b48d8cd56e53ffb90d
jujulilia <jujuscosta07@gmail.com>
2024-04-02 15:52:10 -0300

    Atualization

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import CriarMenu from './src/Interfaces/Menu';import CriarLogin from './src/Interfaces/CadastroView';
+import CriarMenu from './src/assets/Menu';
+import CriarLogin from './src/assets/CadastroView';
+import CadastroProduto from './src/screens/CadastroProduto';
  ;
 
 
 function App(): React.JSX.Element {
   return (
-   <CriarMenu/>
+   //<CriarMenu/>
    //<CriarLogin/>
+   <CadastroProduto/>
   );
 }
 
